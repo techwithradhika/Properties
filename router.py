e0364c4cf61d81ebb6e7d0ea5c172deaf45c45df
techwithradhika <radhikam406@gmail.com>
2024-05-17 01:20:34 +0530

    updated

--- router.py
+++ router.py
@@ -6,14 +6,15 @@ from models import Property
 
 property_router = APIRouter() 
 
-@property_router.post('/create_new_property', response_model=PropertyDetail)
+@property_router.post('/create_new_property', response_model=PropertyList)
 async def create_new_property(property: Property):
     property_dict = property.dict()
     property_id = collection.insert_one(property_dict).inserted_id
     inserted_property = collection.find_one({"_id": property_id})
     if not inserted_property:
         raise HTTPException(status_code=500, detail="Failed to create property")
-    return PropertyDetail(id=str(inserted_property['_id']), **inserted_property)
+    property_list = get_all_properties()    
+    return PropertyList(properties=property_list)
 
 @property_router.get('/fetch_property_details', response_model=PropertyList)
 async def fetch_property_details(city: str):
@@ -25,7 +26,7 @@ async def fetch_property_details(city: str):
         property_list.append(PropertyDetail(id=str(property['_id']), **property))
     return PropertyList(properties=property_list)
 
-@property_router.put('/update_property_details', response_model=PropertyDetail)
+@property_router.put('/update_property_details', response_model=PropertyList)
 async def update_property_details(property_id: str, property: Property):
     updated_property = collection.find_one_and_update(
         {"_id": ObjectId(property_id)},
@@ -34,7 +35,8 @@ async def update_property_details(property_id: str, property: Property):
     )
     if not updated_property:
         raise HTTPException(status_code=404, detail="Property not found")
-    return PropertyDetail(id=str(updated_property['_id']),**updated_property)
+    property_list = get_all_properties()    
+    return PropertyList(properties=property_list)
 
 @property_router.get('/find_cities_by_state')
 async def find_cities_by_state(state: str):
@@ -54,3 +56,12 @@ async def find_similar_properties(property_id: str):
     for property in similar_properties:
         property_list.append(PropertyDetail(id=str(property['_id']), **property))
     return PropertyList(properties=property_list)
+
+def get_all_properties():
+    all_properties = collection.find({})
+    property_list = []
+    for prop in all_properties:
+        prop['_id'] = str(prop['_id']) 
+        prop['id'] = prop.pop('_id')
+        property_list.append(PropertyDetail(**prop))
+    return property_list
